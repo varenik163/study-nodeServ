db8394cd140b990b767138ce9b674b51553263ce
var <varenik16@gmail.com>
2019-01-05 13:22:20 +0400

    +front

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cons = require('consolidate');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var getUser = require('./routes/getUser');
 
 var app = express();
 
 // view engine setup
+app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,9 +24,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'frontend/study-front/build/static')));
+app.use(express.static(path.join(__dirname, 'frontend/study-front/build')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/user', getUser);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,3 +50,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+console.log('Server is running')
---
+++ frontend/study-front/src/Message.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Custom extends Component {
+
+	render() {
+		return <div>
+			Meesage Yo!
+		</div>
+	}
+}
+export default Custom
+
